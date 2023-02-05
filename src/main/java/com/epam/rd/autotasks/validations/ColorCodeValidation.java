7456c1074b49e90493c4b30fdd63c5bee642751b
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-02-05 19:16:46 +0530

    Done

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -4,12 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
-    public static boolean validateColorCode(String color) {
+    public static boolean validateColorCode(String input) {
+        if (input == null) {
+            return false;
+        }
 
-        // Put your code here
+        Pattern pattern = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+        Matcher matcher = pattern.matcher(input);
 
+        return matcher.matches();
     }
 }
+/*This method uses regular expressions to check if the input
+string matches the pattern of a valid HTML color code, which is either
+a 6-digit hexadecimal value or a 3-digit hexadecimal
+value, and starts with the # symbol. If the input string matches
+this pattern, the method returns true; otherwise, it returns false. */
 
 
 
